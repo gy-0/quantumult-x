932af7300756985369ab14956cff6b82581a7ecd
Gao Yuan <41229059+realgaoyuan@users.noreply.github.com>
2023-01-14 16:52:42 +0800

    https://api.revenuecat.com/v1/receipts
    响应体

---
+++ photos.js
@@ -0,0 +1,42 @@
+{
+  "request_date_ms" : 1673570838364,
+  "request_date" : "2023-01-13T00:47:18Z",
+  "subscriber" : {
+    "non_subscriptions" : {
+
+    },
+    "first_seen" : "2023-01-07T08:41:25Z",
+    "original_application_version" : "221221.1318",
+    "other_purchases" : {
+
+    },
+    "management_url" : "https://apps.apple.com/account/subscriptions",
+    "subscriptions" : {
+      "pixelmator_photo_yearly_v1" : {
+        "original_purchase_date" : "2023-01-07T08:42:05Z",
+        "expires_date" : "2033-01-14T08:42:04Z",
+        "is_sandbox" : false,
+        "refunded_at" : null,
+        "unsubscribe_detected_at" : "2023-01-07T08:43:19Z",
+        "grace_period_expires_date" : null,
+        "period_type" : "trial",
+        "purchase_date" : "2023-01-07T08:42:04Z",
+        "billing_issues_detected_at" : null,
+        "ownership_type" : "PURCHASED",
+        "store" : "app_store",
+        "auto_resume_date" : null
+      }
+    },
+    "entitlements" : {
+      "pixelmator_photo_pro_access" : {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "2023-01-07T08:42:04Z",
+        "product_identifier" : "pixelmator_photo_yearly_v1",
+        "expires_date" : "2033-01-14T08:42:04Z"
+      }
+    },
+    "original_purchase_date" : "2023-01-07T08:40:43Z",
+    "original_app_user_id" : "$RCAnonymousID:901f76e9c02942d998bab3f93d8941b5",
+    "last_seen" : "2023-01-12T10:59:48Z"
+  }
+}
